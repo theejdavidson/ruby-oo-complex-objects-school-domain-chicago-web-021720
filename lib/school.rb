404480d450497865e028e0ea4b00ac86534ac289
Ethan Davidson <theejdavidson@gmail.com>
2020-02-19 21:23:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -18,7 +18,7 @@ end
 
 school = School.new("Bayside High School")
 school.add_student("AC Slater", 9)
-school.add_student("Grade Niner", 9)
+school.add_student("Zach Morris", 9)
 school.add_student("Kelly Kapowski", 10)
 school.add_student("Screech", 11)
 puts school.roster
